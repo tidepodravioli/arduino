12af11268e48631160b61af6be6ca70ab58a26c8
tidepodravioli <46001759+tidepodravioli@users.noreply.github.com>
2020-02-25 15:30:24 -0600

    Add files via upload

--- rawry_xd/rawry_xd.ino
+++ rawry_xd/rawry_xd.ino
@@ -1,4 +1,6 @@
-const int str = B00100;
+#include <Servo.h>
+
+const int straight = B00100;
 const int left = B11100;
 const int right = B00111;
 const int full = B11111;
@@ -6,36 +8,58 @@ const int empty = B00000;
 
 enum direc{LEFT,RIGHT,STRAIGHT,INCH,R_INCH};
 
+Servo Lservo;
+Servo Rservo;
 
+String serialQueue = "";
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Init started..");
   
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+
+  Lservo.attach(3);
+  Rservo.attach(2);
+
+  Lservo.write(90);
+  Rservo.write(90);
+
+  Serial.println("Init finished.");
 }
 
 void loop() {
-  int current = mes();
+   int current = 0;
+   for (int i=0; i < 10; i++) {
+   current = current + mes();
+   }
+   current = current/10;
+  serialQueue += (int2bin(current));
   
   if(current == left){
+    serialQueue += ("   LEFT");
     mov(LEFT);
   }
   else if(current == right){
-    if(decision()){
-      mov(STRAIGHT);
-    }
-    else{
-      mov(RIGHT);
-    }
+    serialQueue += ("   RIGHT");
+    mov(RIGHT);
   }
   else if(current == full){
     if(win()){
       
     }
     else{
-      mov(R_INCH);
+      mov(INCH);
       mov(LEFT);
     }
   }
-  else{
-    
+  else if(current == straight){
+    mov(STRAIGHT);
   }
+
+  Serial.println(serialQueue);
+  serialQueue = "";
 }
--- rawry_xd/servomethods.ino
+++ rawry_xd/servomethods.ino
@@ -4,15 +4,29 @@ void mov(direc dir)
 {
   switch(dir){
     case INCH:
+      Lservo.write(40);
+      Rservo.write(140);
     break;
 
     case STRAIGHT:
+      Lservo.write(40);
+      Rservo.write(140);
     break;
 
     case LEFT:
+      Lservo.write(140);
+      Rservo.write(140);
+      delay(10);
+      Lservo.write(90);
+      Rservo.write(90);
     break;
 
     case RIGHT:
+      Lservo.write(40);
+      Rservo.write(40);
+      delay(10);
+      Lservo.write(90);
+      Rservo.write(90);
     break;
   }
 }
@@ -26,11 +40,43 @@ bool decision()
 
 bool win()
 {
-  mov(INCH);
-  
+  return false;
 }
 
 int mes()
 {
-  return empty;
+  int raw[5];
+  int result = 0;
+  
+  raw[0] = analogRead(A1);
+  raw[1] = analogRead(A2);
+  raw[2] = analogRead(A3);
+  raw[3] = analogRead(A4);
+  raw[4] = analogRead(A5);
+
+  const int threshhold = 800;
+  if(raw[0] < threshhold) result += B10000;
+  if(raw[1] < threshhold) result += B01000;
+  if(raw[2] < threshhold) result += B00100;
+  if(raw[3] < threshhold) result += B00010;
+  if(raw[4] < threshhold) result += B00001;
+
+  /*String printsen = "";
+  for(int cur = 0; cur <= 4; cur++){
+    if(raw[cur] < 600){
+      printsen = printsen + "⏺  ";
+    }
+    else{
+      printsen = printsen + "◯  ";
+    }
+  }*/
+  return result;
+}
+
+char * int2bin(unsigned int x)
+{
+ static char buffer[6];
+ for (int i=0; i<6; i++) buffer[4-i] = '0' + ((x & (1 << i)) > 0);
+ buffer[5] ='\0';
+ return buffer;
 }
